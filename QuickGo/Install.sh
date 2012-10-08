8b0a45af6229bde874a8da7407f909e8d5d403fb
Ato ARAKI <ato.araki@gmail.com>
2012-10-08 20:46:37 +0900

    Fix install script

--- QuickGo/Install.sh
+++ QuickGo/Install.sh
@@ -6,6 +6,9 @@
 #  Created by Ato ARAKI on 2012/10/08.
 #  Copyright (c) 2012 Ato ARAKI. All rights reserved.
 
-mkdir ~/Library/QuickLook/
-cp QuickGo.qlgenerator* ~/Library/QuickLook/
+if [ ! -d ~/Library/QuickLook/ ]
+then
+    mkdir -p ~/Library/QuickLook/
+fi
+cp -r QuickGo.qlgenerator* ~/Library/QuickLook/
 /usr/bin/qlmanage -r
