0c76384bb1f6ab573e0e41c3d407e0b0d7661c99
Ato ARAKI <ato.araki@gmail.com>
2012-10-08 20:21:59 +0900

    Add script for install

---
+++ QuickGo/Install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  Install.sh
+#  QuickGo
+#
+#  Created by Ato ARAKI on 2012/10/08.
+#  Copyright (c) 2012 Ato ARAKI. All rights reserved.
+
+mkdir ~/Library/QuickLook/
+cp QuickGo.qlgenerator* ~/Library/QuickLook/
+/usr/bin/qlmanage -r
---
+++ QuickGo/Uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  Uninstall.sh
+#  QuickGo
+#
+#  Created by Ato ARAKI on 2012/10/08.
+#  Copyright (c) 2012 Ato ARAKI. All rights reserved.
+
+rm -rf ~/Library/QuickLook/QuickGo.qlgenerator*
